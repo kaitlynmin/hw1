3998800a685abd9931aa004340fa5833f7ec7e81
Kaitlyn Min <kimin@usc.edu>
2024-01-27 02:53:55 +0000

    part 2 finished

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,25 @@
 
 int main(int argc, char* argv[])
 {
+ ULListStr list;
 
+    // list.push_back("fred");
+    // list.push_front("bob");
+
+    // std::cout << "first list\n";
+    // for (size_t i = 0; i < list.size(); ++i) {
+    //     std::cout << list.get(i) << " ";
+    // }
+    // std::cout<<std::endl;
+
+    list.push_front("bob");
+    list.pop_back();
+
+    std::cout << "list\n";
+    for (size_t i = 0; i < list.size(); ++i) {
+        std::cout << list.get(i) << " ";
+        std::cout << list.size();
+    }
+    std::cout<<std::endl;
+    return 0;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -26,6 +26,118 @@ size_t ULListStr::size() const
 
 // WRITE YOUR CODE HERE
 
+
+// end of helper functions
+void ULListStr::push_back(const std::string& val){
+  if (this->empty() || (tail_ && tail_->last == ARRSIZE)) {
+    Item* newItem = new Item();
+    newItem->val[newItem->last++] = val;
+
+    if (this->empty()) {
+      newItem->prev = nullptr;
+      newItem->next = nullptr;
+      head_ = newItem;
+      tail_ = newItem;
+    } else {
+      newItem->prev = tail_;
+      newItem->next = nullptr;
+      tail_->next = newItem;
+      tail_= newItem;
+    }
+  } else if (tail_->last < ARRSIZE) {
+      tail_->val[tail_->last++] = val;
+  }
+  size_++;
+}
+
+// only allocate new item if current head has no space in front of first
+ void ULListStr::push_front(const std::string& val) {
+  if (this->empty() || head_->first == 0) {
+    Item* newItem = new Item();
+    newItem->val[ARRSIZE - 1] = val;
+    newItem->first = ARRSIZE - 1;
+    newItem->next = head_;
+    newItem->last = ARRSIZE;
+    newItem->prev = nullptr;
+    
+    if (!this->empty()) {
+      head_->prev = newItem;
+    } else {
+      tail_ = newItem;
+    }
+    head_ = newItem;
+  } else {
+    --head_->first;
+    head_->val[head_->first] = val;
+  }
+  size_++;
+}
+
+void ULListStr::pop_back() {
+    if (this->empty()) {
+      return;
+    } else if (tail_->last == 0) {
+      Item* temp = tail_;
+      tail_ = tail_->prev;
+
+      if (tail_) {
+        tail_->next = nullptr;
+      } else {
+        head_=nullptr;
+      }
+      if (head_) {
+      delete temp;
+      size_--;
+      }
+    } else {
+      tail_->last--;
+      size_--;
+    }
+}
+
+void ULListStr::pop_front() {
+  if (this->empty()) {
+    return;
+  } 
+  size_--;
+  if (head_->first == ARRSIZE - 1) {
+    Item* temp = head_;
+    head_ = head_->next;
+    if (head_) {
+      head_->prev = nullptr;
+    }
+    delete temp;
+  } else {
+    head_->first++;
+  }
+  if (this->empty()) {
+    tail_ = NULL;
+  }
+}
+
+std::string const & ULListStr::back() const {
+  return tail_->val[tail_->last -1];
+}
+
+
+std::string const & ULListStr::front() const {
+  return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+  Item* temp = head_;
+  while (temp != nullptr) {
+    if (loc < temp->last - temp->first) {
+      size_t indexInArray = temp->first +loc;
+      return &(temp->val[indexInArray]);
+    }
+    loc = loc - (temp->last - temp->first);
+    temp = temp->next;
+  }
+  return nullptr;
+}
+
+// DO NOT TOUCH
 void ULListStr::set(size_t loc, const std::string& val)
 {
   std::string* ptr = getValAtLoc(loc);
@@ -63,3 +175,9 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+
+// if list empty
+  // point head to new node
+  // if list exists
+  // point tail to new node
